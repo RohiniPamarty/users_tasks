6151c6a26e434fee5af148988d46d046dbbe8814
Rohini <pvrohini86@gmail.com>
2012-08-03 16:37:46 +0530

    sendmail changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,16 +36,12 @@ config.action_mailer.default_url_options = { :host => '0.0.0.0:3000' }
 
   # Expands the lines which load the assets
   config.assets.debug = true
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'gmail.com',
-  :user_name            => 'rubyonrails.tasks@gmail.com',
-  :password             => 'rubyonrails',
-  :authentication       => :login,
-  :enable_starttls_auto => true  }
- 
+config.action_mailer.delivery_method = :sendmail
+
+config.action_mailer.sendmail_settings = {
+:location => '/usr/sbin/sendmail',
+:arguments => '-i -t'
+}
 
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
